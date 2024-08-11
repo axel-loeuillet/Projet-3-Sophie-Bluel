327fdfebbbc3267c264cc413611fe6839019b338
Axel Loeuillet <aloeuillet93@gmail.com>
2024-08-12 00:16:23 +0200

    return works from API

---
+++ FrontEnd/index.js
@@ -0,0 +1,8 @@
+//RÉCUPÉRATION DES PROJETS DEPUIS L'API
+const works = await getWorks()
+function getWorks() {
+    return fetch('http://localhost:5678/api/works')
+        .then(response => response.json())
+        .catch(error => ('Error:', error));
+}
+console.log(works)
