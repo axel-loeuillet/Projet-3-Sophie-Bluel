bb0c15e3935b577eac4dd5bc04bf57f4d1d93d03
Axel Loeuillet <aloeuillet93@gmail.com>
2024-08-12 00:22:55 +0200

    add works in gallery from javascript

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -6,3 +6,20 @@ function getWorks() {
         .catch(error => ('Error:', error));
 }
 console.log(works)
+
+//AFFICHAGE DES PROJETS DANS LA GALERIE
+function genererWorks(works) {
+    for (let i = 0; i < works.length; i++) {
+        const data = works[i];
+        let gallery = document.querySelector(".gallery");
+        let figure = document.createElement("figure");
+        let img = document.createElement("img");
+        let figCaption = document.createElement("figcaption");
+        figCaption.textContent = data.title;
+        img.src = data.imageUrl;
+        figure.appendChild(img);
+        figure.appendChild(figCaption);
+        gallery.appendChild(figure);
+    }
+}
+genererWorks(works)
