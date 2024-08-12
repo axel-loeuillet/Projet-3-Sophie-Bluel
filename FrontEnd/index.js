34a172c9be401579df23fc6512ca76aca70c1f73
Axel Loeuillet <aloeuillet93@gmail.com>
2024-08-12 16:05:45 +0200

    Filter of projets for each category

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -7,8 +7,9 @@ function getWorks() {
 }
 console.log(works)
 
+
 //AFFICHAGE DES PROJETS DANS LA GALERIE
-function displayWorks(works) {
+function genererWorks(works) {
     for (let i = 0; i < works.length; i++) {
         const data = works[i];
         let gallery = document.querySelector(".gallery");
@@ -22,41 +23,68 @@ function displayWorks(works) {
         gallery.appendChild(figure);
     }
 }
-displayWorks(works)
+genererWorks(works)
 
-//RÉCUPÉRATION DES CATEGORIES DEPUIS L'API
+//Récupération des catégories depuis l'API
 const categories = await getListOfCategories()
 console.log(categories)
+
 function getListOfCategories() {
     return fetch('http://localhost:5678/api/categories')
         .then(response => response.json())
         .catch(error => ('Error:', error));
 }
 
-//CREATION DU BOUTON "TOUS"
+//création bouton Tous
 let category = document.querySelector(".category");
-console.log(category)
 let btnTous = document.createElement("button");
 btnTous.textContent = "Tous";
 btnTous.setAttribute("data-categorie-id", "Tous");
-category.appendChild(btnTous);
+category.appendChild(btnTous)
 btnTous.addEventListener("click", function () {
 })
 
-//AFFICHAGE DES BOUTONS DE CATEGORIE DANS LA GALERIE
-function displayListOfCategories(categories) {
-    for (let i = 0; i < categories.length; i++) {
-        const categorie = categories[i];
-        let btn = document.createElement("button");
-        btn.textContent = categorie.name;
-        btn.classList.add("category-btn")
-        btn.setAttribute("data-categorie-id", categorie.id)
-        category.appendChild(btn)
-        console.log(btn)
-        btn.addEventListener("click", function () {
-        })
-    }
+
+//creation des boutons de catégories
+for (let i = 0; i < categories.length; i++) {
+    const categorie = categories[i];
+    let btn = document.createElement("button");
+    btn.textContent = categorie.name;
+    btn.classList.add("category-btn")
+    btn.setAttribute("data-categorie-id", categorie.id)
+    category.appendChild(btn)
+    btn.addEventListener("click", function () {
+    })
 }
-displayListOfCategories(categories)
 
+//affichage de tous les projets dans la catégorie "Tous"
+const btnIdTous = document.querySelector('[data-categorie-id="Tous"]');
+btnIdTous.addEventListener("click", function () {
+    const filtreIdTous = works.filter(function (works) {
+        return works.categoryId === 1, 2, 3;
+    })
+    document.querySelector(".gallery").innerHTML = "";
+    genererWorks(filtreIdTous);
+});
 
+//filtrage des projets par catégories
+const btnId1 = document.querySelector('[data-categorie-id="1"]');
+const buttons = document.querySelectorAll(".category-btn")
+btnId1.addEventListener("click", function () {
+    const filtreId1 = works.filter(function (works) {
+        return works.categoryId === 1;
+    })
+    document.querySelector(".gallery").innerHTML = "";
+    genererWorks(filtreId1);
+});
+
+buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        const dataCategoryId = button.getAttribute("data-categorie-id")
+        const filtreId1 = works.filter(function (works) {
+            return works.categoryId === Number(dataCategoryId);
+        })
+        document.querySelector(".gallery").innerHTML = "";
+        genererWorks(filtreId1);
+    })
+});
